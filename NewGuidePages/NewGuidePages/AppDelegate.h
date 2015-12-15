968e87ac850a635531ab3e3d99aff4584555be97
jinxing680 <baijinxin680@163.com>
2015-12-15 13:39:17 +0800

    初始化工程

---
+++ NewGuidePages/NewGuidePages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewGuidePages
+//
+//  Created by 白金星 on 15/12/15.
+//  Copyright © 2015年 cn.bjx680. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewGuidePages/NewGuidePages/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  NewGuidePages
+//
+//  Created by 白金星 on 15/12/15.
+//  Copyright © 2015年 cn.bjx680. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"项目初始化");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
